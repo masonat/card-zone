af2c68e3ff3d6d47b3efed4173942d5222274460
Mason Turner <69993389+masonat@users.noreply.github.com>
2022-05-03 20:12:45 -0600

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/"> Card Zone </a>
+            </header>
+            <main>List Products</main>
+        </div>
+    );
+}
+
+export default App;
