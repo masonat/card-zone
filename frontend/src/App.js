2b7e8c19f4cb6665f357cc4d47e28a370c1b8f25
Mason Turner <69993389+masonat@users.noreply.github.com>
2022-05-03 20:37:41 -0600

    add images and featured products screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
+import data from "./data";
 
 function App() {
-    return (
-        <div>
-            <header>
-                <a href="/"> Card Zone </a>
-            </header>
-            <main>List Products</main>
-        </div>
-    );
+	return (
+		<div>
+			<header>
+				<a href='/'> Card Zone </a>
+			</header>
+			<main>
+				<h1>Featured Products</h1>
+				<div className='products'>
+					{data.products.map((product) => (
+						<div className='product' key={product.slug}>
+							<a href={`/products/${product.slug}`}>
+								<img src={product.image} alt={product.name} />
+							</a>
+							<div className='product-info'>
+								<a href={`/products/${product.slug}`}>
+									<p>{product.name}</p>
+								</a>
+								<p><strong>${product.price}</strong></p>
+								<button>Add to Cart</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
