b030f359248c0e6463f2e3cfb5c5cd9cb48ba037
Mason Turner <69993389+masonat@users.noreply.github.com>
2022-05-04 02:21:04 -0600

    added MongoDB connection

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 const data = {
 	products: [
 		{
-			_id: 1,
+			// _id: 1,
 			name: "Jihyo Feel Special",
 			slug: "jihyo-feel-special",
 			category: "Unnie",
@@ -14,7 +14,7 @@ const data = {
 			description: "high quality shirt",
 		},
 		{
-			_id: 2,
+			//_id: 2,
 			name: "Mina Feel Special",
 			slug: "mina-feel-special",
 			category: "Unnie",
@@ -27,7 +27,7 @@ const data = {
 			description: "high quality product",
 		},
 		{
-			_id: 3,
+			//_id: 3,
 			name: "Chaeyoung Feel Special",
 			slug: "chaeyoung-feel-special",
 			category: "Maknae",
@@ -40,7 +40,7 @@ const data = {
 			description: "high quality product",
 		},
 		{
-			_id: 4,
+			//_id: 4,
 			name: "Tzuyu Feel Special",
 			slug: "tzuyu-feel-special",
 			category: "Maknae",
@@ -53,7 +53,7 @@ const data = {
 			description: "high quality product",
 		},
 		{
-			_id: 5,
+			//_id: 5,
 			name: "Sana Formula of Love POB",
 			slug: "sana-fol-pob",
 			category: "Unnie",
@@ -66,7 +66,7 @@ const data = {
 			description: "high quality product",
 		},
 		{
-			_id: 5,
+			//_id: 6,
 			name: "Nayeon Taste of Love Monograph",
 			slug: "nayeon-tol-mono",
 			category: "Unnie",
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
 
 app.get("/api/products", (req, res) => {
 	res.send(data.products);
