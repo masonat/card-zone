054e6c8a218a0b6ce3aabb971334f7e4bd5e25e5
Mason Turner <69993389+masonat@users.noreply.github.com>
2022-05-04 00:54:54 -0600

    app.js add cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Container, Navbar } from "react-bootstrap";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Container, Navbar, Badge, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Footer from "./components/Footer";
+import { useContext } from "react";
+import { Store } from "./Store";
+
 
 function App() {
+	const { state } = useContext(Store);
+	const { cart } = state;
 	return (
 		<BrowserRouter>
 			<div className='d-flex flex-column site-container'>
@@ -17,6 +22,16 @@ function App() {
 									<i className='fa-solid fa-house'></i> Card Zone
 								</Navbar.Brand>
 							</LinkContainer>
+							<Nav className='me-auto'>
+								<Link to='/cart' className='nav-link'>
+									Cart
+									{cart.cartItems.length > 0 && (
+										<Badge pill bg='danger'>
+											{cart.cartItems.length}
+										</Badge>
+									)}
+								</Link>
+							</Nav>
 						</Container>
 					</Navbar>
 				</header>
