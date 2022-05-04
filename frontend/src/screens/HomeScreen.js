05f084d65d32e0437aecb7893639ca95032d04c0
Mason Turner <69993389+masonat@users.noreply.github.com>
2022-05-03 22:00:31 -0600

    implement fetching product data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
-import data from "../data";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomeScreen() {
+	const [products, setProducts] = useState([]); // returns array containing a variable and function to update variable
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios.get("/api/products");
+			setProducts(result.data);
+		};
+		fetchData();
+	}, []);
 	return (
 		<div>
 			<h1>Featured Products</h1>
 			<div className='products'>
-				{data.products.map((product) => (
+				{products.map((product) => (
 					<div className='product' key={product.slug}>
 						<Link to={`/product/${product.slug}`}>
 							<img src={product.image} alt={product.name} />
@@ -16,7 +25,10 @@ function HomeScreen() {
 								<p>{product.name}</p>
 							</Link>
 							<p>
-								<strong><i className="fa-solid fa-dollar-sign"></i> {product.price}</strong>
+								<strong>
+									<i className='fa-solid fa-dollar-sign'></i>{" "}
+									{product.price}
+								</strong>
 							</p>
 							<button>Add to cart</button>
 						</div>
